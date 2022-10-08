b4487d2b14f8b6b7b570be65fdcfba8b96175f61
Booruno <1brunoocunha@gmail.com>
2022-10-08 20:40:11 -0300

    Section Sobre Mim

---
+++ files/script.js
@@ -0,0 +1,6 @@
+new Readmore(".sobre-container", {
+  speed: 100,
+  collapsedHeight: 300,
+  moreLink: '<a class="btn-leia">Ler mais ...</a>',
+  lessLink: '<a class="btn-leia">Ler menos ...</a>',
+});
